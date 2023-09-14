b4a75886c618dc28ec5faf35f8d18136b7b831ba
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-09-14 10:34:04 +0200

    add click listener on btn create menu

--- app/src/main/java/com/example/newexperience/SettingsActivity.java
+++ app/src/main/java/com/example/newexperience/SettingsActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -13,5 +16,9 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Intent i = getIntent();
+    }
+
+    public void createMenu(View theView){
+        ((TextView)findViewById(R.id.textView5)).setText("Hello");
     }
 }
